270545d0f7e52b435165416a8c921735446ecc16
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2025-01-27 19:51:06 -0300

    feat: :sparkles: <mobile> adicionando menu hambúrguer

--- app/main.js
+++ app/main.js
@@ -1,3 +1,17 @@
 // const conselho = await fetch("https://api.adviceslip.com/advice");
 // const conselhoObjeto = await conselho.json();
 // console.log(conselhoObjeto);
+const menu = document.querySelector(".menu__botao");
+const menuLista = document.querySelector(".menu__lista");
+const menuImagem = menu.querySelector("img");
+
+menu.addEventListener("click", () => {
+  const isExpanded = menuLista.classList.toggle("ativo");
+  menuLista.setAttribute("aria-expanded", isExpanded);
+
+  if (isExpanded) {
+    menuImagem.src = "assets/icons-black/menu-black-open.svg";
+  } else {
+    menuImagem.src = "assets/icons-black/menu-black-close.svg";
+  }
+});
