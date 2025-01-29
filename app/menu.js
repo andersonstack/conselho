a3545c2197b47242f5725e9514d8069a2e2e4164
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2025-01-29 17:35:34 -0300

    style: :wheelchair: adicionando melhor animação para marcar clique

--- app/menu.js
+++ app/menu.js
@@ -2,6 +2,7 @@ export function menuNav() {
   const menu = document.querySelector(".menu__botao");
   const menuLista = document.querySelector(".menu__lista");
   const menuImagem = menu.querySelector("img");
+  menuImagem.style.transition = "transform 0.3s ease-out";
   const main = document.querySelector("main");
 
   menu.addEventListener("click", () => {
@@ -9,9 +10,10 @@ export function menuNav() {
     menuLista.setAttribute("aria-expanded", isExpanded);
 
     if (isExpanded) {
-      menuImagem.src = "assets/icons-black/menu-black-open.svg";
+      menuImagem.style.transform = "scale(1.2, 1.5)";
       main.style.opacity = 0.5;
     } else {
+      menuImagem.style.transform = "scale(1, 1)";
       menuImagem.src = "assets/icons-black/menu-black-close.svg";
       main.style.opacity = 1;
     }
