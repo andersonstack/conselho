846945243dc865798d5d639d92950e1dacd57d1d
andersonstack <andersong.pereiracruz@gmail.com>
2025-01-31 10:51:36 -0300

    refactor: :recycle: melhorando explicação do código

--- app/controller/conselhoNavController.js
+++ app/controller/conselhoNavController.js
@@ -1,15 +1,20 @@
 function menuNav() {
+  // Seleção de elementos
   const menu = document.querySelector(".menu__botao");
   const menuLista = document.querySelector(".menu__lista");
   const menuImagem = menu.querySelector("img");
-  menuImagem.style.transition = "transform 0.3s ease-out";
   const main = document.querySelector("main");
   const menuPerfil = document.querySelector(".menu__botao-usuario");
+  const logout = document.querySelector(".logout");
+  const botaoLogout = document.querySelector(".logout");
 
-  menuPerfil.addEventListener("click", () => {
-    const logout = document.querySelector(".logout");
+  // Inicializando a transição da imagem do menu
+  menuImagem.style.transition = "transform 0.3s ease-out";
 
-    // Alternar entre visível e invisível
+  // Alterna visibilidade do menu de logout ao clicar no perfil
+  menuPerfil.addEventListener("click", (event) => {
+    event.stopPropagation(); // Impede que o clique no menu de logout feche o menu
+
     if (logout.style.visibility === "visible") {
       logout.style.visibility = "hidden";
       logout.style.transform = "translateY(100%)"; // Esconde com efeito
@@ -19,11 +24,12 @@ function menuNav() {
     }
   });
 
-  const botaoLogout = document.querySelector(".logout");
+  // Redireciona ao clicar no botão de logout
   botaoLogout.addEventListener("click", () => {
     window.location.href = "../screens/login.html";
   });
 
+  // Abre ou fecha o menu de navegação ao clicar no botão de menu
   menu.addEventListener("click", () => {
     const isExpanded = menuLista.classList.toggle("ativo");
     menuLista.setAttribute("aria-expanded", isExpanded);
@@ -36,6 +42,12 @@ function menuNav() {
       menuImagem.src = "assets/icons-black/menu-black-close.svg";
       main.style.opacity = 1;
     }
+  });
+
+  // Fecha o menu de logout se clicar fora
+  document.addEventListener("click", () => {
+    logout.style.visibility = "hidden";
+    logout.style.transform = "translateY(100%)"; // Esconde com efeito
   });
 }
 
