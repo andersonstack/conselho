3197b507e5364463ab4ddb75c8396974988f1303
andersonstack <andersong.pereiracruz@gmail.com>
2025-02-01 10:15:24 -0300

    feat: :sparkles: adicionando conexão de cadastro com backend

--- src/app/controller/cadastroController.js
+++ src/app/controller/cadastroController.js
@@ -1,4 +1,19 @@
+import { api } from "../services/backend.js";
+
 const botaoPOST = document.querySelector("#button__post");
-botaoPOST.addEventListener("click", () => {
-  console.log("fui clicado");
+botaoPOST.addEventListener("click", async () => {
+  await api
+    .post("/users", {
+      userName: document.querySelector("#nome").value,
+      name: document.querySelector("#nome_usuario").value,
+      senha: document.querySelector("#senha").value,
+      frases: {},
+    })
+    .then((response) => {
+      if (response.status === 201) {
+        window.location.href = "../../index.html";
+      } else {
+        alert("Erro ao cadastrar usuário");
+      }
+    });
 });
