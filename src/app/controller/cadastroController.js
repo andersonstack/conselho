618c0f112cc4674e2cdcc9f8d9af3e5c8c8b23ac
andersonstack <andersong.pereiracruz@gmail.com>
2025-02-01 10:35:38 -0300

    fix: :bug: melhroando o feedback para o usuário

--- src/app/controller/cadastroController.js
+++ src/app/controller/cadastroController.js
@@ -1,19 +1,26 @@
 import { api } from "../services/backend.js";
 
 const botaoPOST = document.querySelector("#button__post");
+
 botaoPOST.addEventListener("click", async () => {
-  await api
-    .post("/users", {
+  try {
+    const response = await api.post("/users", {
       userName: document.querySelector("#nome").value,
       name: document.querySelector("#nome_usuario").value,
       senha: document.querySelector("#senha").value,
       frases: {},
-    })
-    .then((response) => {
-      if (response.status === 201) {
-        window.location.href = "../../index.html";
-      } else {
-        alert("Erro ao cadastrar usuário");
-      }
     });
+
+    // Verificando a resposta bem-sucedida
+    if (response.status === 201) {
+      window.location.href = "sucesso.html";
+    } else if (response.status === 504) {
+      // Caso o erro de usuário já existente
+      window.location.href = "erro.html";
+    }
+  } catch (error) {
+    // Captura qualquer erro de rede ou outros problemas
+    console.error("Erro ao realizar o cadastro:", error);
+    window.location.href = "erro.html"; // Redireciona para a página de erro
+  }
 });
