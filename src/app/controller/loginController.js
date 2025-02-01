76bd3b00ec292a3273fea70961d8084b5615f014
andersonstack <andersong.pereiracruz@gmail.com>
2025-02-01 11:01:54 -0300

    feat: :sparkles: adicionando método de login

--- src/app/controller/loginController.js
+++ src/app/controller/loginController.js
@@ -1,14 +1,31 @@
+import { api } from "../services/backend.js";
+
 const botaoLogin = document.querySelector(".button__login");
 const paragrafoError = document.querySelector(".aviso__login");
 
 botaoLogin.addEventListener("click", () => {
-  const autenticado = false;
-  if (autenticado) {
-    window.location.href = "src/screens/home.html";
-  } else {
-    paragrafoError.style.display = "block";
-  }
+  const username = document.querySelector("#nome_usuario").value;
+  const senha = document.querySelector("#senha").value;
+
+  api
+    .post("/login", {
+      userName: username,
+      senha: senha,
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        window.location.href = "src/screens/home.html";
+      } else if (response.status === 401) {
+        paragrafoError.textContent = "Senha inválida.";
+        paragrafoError.style.display = "block";
+      }
+    })
+    .catch((error) => {
+      console.error("Erro ao realizar login:", error);
+      paragrafoError.style.display = "block";
+    });
 });
+
 const botaoCadastrar = document.querySelector(".button__sigup");
 botaoCadastrar.addEventListener("click", () => {
   window.location.href = "src/screens/cadastro.html";
