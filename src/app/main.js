501b5d407db4b81353ee5fb67e76bf1b51f37d58
andersonstack <andersong.pereiracruz@gmail.com>
2025-02-02 09:17:22 -0300

    feat: :sparkles: adicionando api de tradução

--- src/app/main.js
+++ src/app/main.js
@@ -7,17 +7,19 @@ const apiConselho = await conselhoService.conselhoAPI();
 
 if (apiConselho) {
   const textoAPI = apiConselho.slip.advice;
+  const textoTraduzido = await conselhoService.traduzirTexto(textoAPI);
+  console.log(textoTraduzido);
   const textoID = apiConselho.slip.id;
 
   const mainConselho = document.getElementById("cartao__texto");
-  mainConselho.textContent = textoAPI;
+  mainConselho.textContent = textoTraduzido;
 
   var typewriter = new Typewriter(mainConselho, {
     loop: false,
     delay: 75,
   });
 
-  typewriter.typeString(textoAPI).start();
+  typewriter.typeString(textoTraduzido).start();
 
   conselhoNavController.menuNav();
 
@@ -25,6 +27,6 @@ if (apiConselho) {
 
   listaConselhosController.listaConselhos();
   bookmark.addEventListener("click", () =>
-    conselhoController.salvarConselho(textoAPI, textoID)
+    conselhoController.salvarConselho(textoTraduzido, textoID)
   );
 }
--- src/app/services/conselhoApi.js
+++ src/app/services/conselhoApi.js
@@ -1,3 +1,21 @@
+async function traduzirTexto(texto) {
+  const url = `https://translation.googleapis.com/language/translate/v2?key=AIzaSyC49W9VxHaxiPJ2m7GLrpMC0eO8d8Vhe8I`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      q: texto,
+      target: "pt", // Código do idioma para português
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+  return data.data.translations[0].translatedText;
+}
+
 async function conselhoAPI() {
   try {
     const conselho = await fetch("https://api.adviceslip.com/advice");
@@ -11,4 +29,5 @@ async function conselhoAPI() {
 
 export const conselhoService = {
   conselhoAPI,
+  traduzirTexto,
 };
